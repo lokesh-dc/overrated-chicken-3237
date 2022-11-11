ef39e6e0f0334af3e8b34bd5b91579c3a55ba70f
aaryansinha16 <aaryansinha16@gmail.com>
2022-11-11 12:48:18 +0530

    Razorpay Added (with backend)

---
+++ pages/api/razorpay.ts
@@ -0,0 +1,41 @@
+const Razorpay = require("razorpay");
+const shortid = require("shortid");
+
+export default async function handler(req:any, res:any) {
+  if (req.method === "POST") {
+    // Initialize razorpay object
+    const razorpay = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY,
+      key_secret: process.env.RAZORPAY_SECRET,
+    });
+
+    var total:any = JSON.parse(req.body)
+    console.log(JSON.parse(req.body), "REQ BODY")
+
+    // Create an order -> generate the OrderID -> Send it to the Front-end
+    // Also, check the amount and currency on the backend (Security measure)
+    const payment_capture = 1;
+    const amount = total.amount;
+    const currency = "INR";
+    const options = {
+      amount: (amount * 100).toString(),
+      currency,
+      receipt: shortid.generate(),
+      payment_capture,
+    };
+
+    try {
+      const response = await razorpay.orders.create(options);
+      res.status(200).json({
+        id: response.id,
+        currency: response.currency,
+        amount: response.amount,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json(err);
+    }
+  } else {
+    // Handle any other HTTP method
+  }
+}

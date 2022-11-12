dbaa4d8cc17514ff3d27177c5875376d7d80c974
lokesh-dc <lokesh.cdewanand@gmail.com>
2022-11-12 20:59:56 +0530

    Mail sending

--- pages/api/users/verify.ts
+++ pages/api/users/verify.ts
@@ -12,8 +12,8 @@ const transporter = nodemailer.createTransport({
     port: 465,
     secure: true,
     auth: {
-        user: "lokesh.cdewanand@gmail.com",
-        pass: "kukhqfbjgzormkkv"
+        user: "",
+        pass: ""
     },
     tls : {
         rejectUnauthorized : false 

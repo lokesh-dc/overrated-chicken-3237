db17d0397949e8efa6a0f50bce2fbc58b2c32c18
Utkarsh <famedustevershine@gmail.com>
2022-11-13 16:27:23 +0530

    Workin on Review

--- pages/api/reviews/index.ts
+++ pages/api/reviews/index.ts
@@ -7,26 +7,32 @@ const jwt = require("jsonwebtoken");
 export default async (req:any, res:any) => {
     try {
         await connect();
-        let { content, productId } = req.body;
+        let { content, productId,rating } = req.body;
+        const {id} =req.headers
+        const date= new Date().toString()
         if(req.method==="GET"){
-            const reviews = await reviewModel.find({ productId }).populate("userId");
+            const reviews = await reviewModel.find({productId:id}).populate("userId");
             res.send(reviews);
         }else {
-            const { token } = req.headers;
-            if(!token){
+            const { cookies } = req;
+            const parseCookie:any= JSON.parse(cookies.mohallaMartJwt)
+            if(!parseCookie.token){
                 return res.status(401).send("Unauthorised Access!")
             }
-            let { id } =  jwt.verify(token, "vdvhsvdsvcdcvsdvcvkc");
+            let { id } =  jwt.verify(parseCookie.token, "vdvhsvdsvcdcvsdvcvkc");
             if(req.method==="POST"){
                 let hasReviewed = await reviewModel.findOne({productId, userId: id});
                 if(!hasReviewed){
-                    await reviewModel.create({ productId, userId: id, content });
+                    await reviewModel.create({ productId, userId: id, content,rating ,date:date});
                     return res.send("Review added Successfully");
                 }else{
                     return res.send("Already reviewed");
                 }
             }else if(req.method==="DELETE"){
-                let hasReviewed = await reviewModel.findOne({productId, userId: id});
+                const { cookies } = req;
+                const parseCookie:any= JSON.parse(cookies.mohallaMartJwt)
+                const Userid =  jwt.verify(parseCookie.token, "vdvhsvdsvcdcvsdvcvkc");
+                let hasReviewed = await reviewModel.findOne({id, userId: Userid.id});
                 if(!hasReviewed){
                     return res.status(401).send("Missing Permissions");
                 }

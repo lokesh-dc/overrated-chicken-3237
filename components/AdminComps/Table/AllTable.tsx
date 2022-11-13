7106d98c53acad51e73cddd68f02f7f4e517047d
aaryansinha16 <aaryansinha16@gmail.com>
2022-11-13 16:08:55 +0530

    viewAll products/users/brands in admin

--- components/AdminComps/Table/AllTable.tsx
+++ components/AdminComps/Table/AllTable.tsx
@@ -1,9 +1,10 @@
 import { Avatar, Table, TableCaption, TableContainer, Tbody, Td, Text, Tfoot, Th, Thead, Tr, VStack } from '@chakra-ui/react'
 import React from 'react'
 
-const AllTable = ({title, role, amount, totalP, user, email, roleHeading}:any) => {
+const AllTable = ({title, role, amount, totalP, user, email, roleHeading, data}:any) => {
 
-    var arr = new Array(20).fill(0)
+    console.log(data, 'daTA')
+    // var arr = new Array(20).fill(0)
 
   return (
     <TableContainer bgColor='rgba(0, 0, 0, 0)' color='white' w='100%' >
@@ -13,41 +14,37 @@ const AllTable = ({title, role, amount, totalP, user, email, roleHeading}:any) =
                 <Th color='white' w='5%'   textAlign='center' isNumeric>No.</Th>
                 <Th color='white' w='30%'  textAlign='center' >{title}</Th>
                 <Th color='white' w='30%'  textAlign='center'>{roleHeading}</Th>
-                <Th color='white' w='15%'  textAlign='center' isNumeric>{totalP}</Th>
+                { title !== "User" || title != "Brands" && <Th color='white' w='15%'  textAlign='center' isNumeric>{totalP}</Th>}
                 <Th color='white' w='15%'  textAlign='center' isNumeric></Th>
             </Tr>
             </Thead>
             <Tbody fontSize='20px'>
-                              
+
             {
-                arr.map((item,j) => {
-                    for(var i = 0; i<20; i++){
-                        return (
-                            <Tr  >
-                                <Td textAlign='center' isNumeric>{j+1}</Td>
-                                <Td textAlign='center' justifyContent='center' alignItems='center' gap={3} display='flex'>
-                                    <Avatar src='https://bit.ly/dan-abramov' borderRadius='2xl'/> 
-                                    <VStack spacing={0}>
-                                        <Text fontSize='20px'>{user}</Text>
-                                        <Text fontSize='14px'>{email}</Text>
-                                    </VStack>
-                                </Td>
-                                <Td textAlign='center'>{role}</Td>
-                                <Td textAlign='center' isNumeric>{amount}</Td>
-                                <Td textAlign='center' >Edit</Td>
-                            </Tr>
-                        )
-                    }
-                })
-            }
+                data?.map((item:any, j:any) => (
+                    <Tr  >
+                        <Td textAlign='center' isNumeric>{j+1}</Td>
+                        <Td textAlign='center' justifyContent='center' alignItems='center' gap={3} display='flex'>
+                            <Avatar src={item.src || item.logo || 'https://bit.ly/dan-abramov'} borderRadius='2xl'/> 
+                            <VStack spacing={0}>
+                                <Text fontSize='20px'>{item?.firstName}{" "}{item?.lastName} {item?.name}{item?.title?.slice(0,16)}</Text>
+                                <Text fontSize='14px'>{item?.email}</Text>
+                            </VStack>
+                        </Td>
+                        <Td textAlign='center'>{item?.role}{`${item?.price}₹`}</Td>
+                        {title != "User" || title != "Brands" && <Td textAlign='center' isNumeric>{amount}</Td>}
+                        <Td textAlign='center' >Edit</Td>
+                    </Tr>
+                ))
+            }   
 
             </Tbody>
             <Tfoot >
             <Tr>
-                <Th color='white'>Total Users : 20</Th>
+                <Th color='white'>Total Users : {data.length}</Th>
                 <Th color='white'></Th>
                 <Th color='white'></Th>
-                <Th color='white' isNumeric>Total Purchase: 50,000Rs</Th>
+                {title != "User" || title != "Brands" && <Th color='white' isNumeric>Total Purchase: 50,000Rs</Th> }
                 <Th color='white'></Th>
             </Tr>
             </Tfoot>
